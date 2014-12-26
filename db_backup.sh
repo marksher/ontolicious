476951c67c54bee6ced90968c401b240f1167bfe
Ubuntu <ubuntu@ip-172-31-40-206.eu-west-1.compute.internal>
2014-12-26 16:39:10 +0000

    gitignore

--- db_backup.sh
+++
@@ -1,5 +0,0 @@
-DOM=$(date +%-d)
-DOM=$((DOM % 7))
-sudo -s mysqldump -u arlanda -parlanda ontolicious > /home/ubuntu/ontolicious-$DOM.sql
-echo "done"
-sudo -s mv /home/ubuntu/ontolicious-$DOM.sql /s3 
